63202e3673731eccdfd84f60dedc6fb4efd5109b
Andrei <g-z-x@bk.ru>
2025-08-14 02:28:20 +0300

    feat: Overwrite remote with current local project state

--- src/electron.d.ts
+++
@@ -1,9 +0,0 @@
-import { Task } from './index';
-
-declare global {
-  interface Window {
-    electronAPI: {
-      showNotification: (options: { title: string; body:string; }) => Promise<boolean>;
-    }
-  }
-}
--- temp_source/dist-electron/preload.js
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-console.log('[Preload] Script loaded.');
-contextBridge.exposeInMainWorld('electronAPI', {
-    showNotification: (options) => {
-        console.log(`[Preload] Calling ipcRenderer.invoke with:`, options);
-        return ipcRenderer.invoke('show-notification', options);
-    }
-});
